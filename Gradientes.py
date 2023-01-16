494f051aee1258cc4d7f4c799f763ae093fc98d7
pocotu <learning1830@outlook.com>
2023-01-16 10:59:28 -0500

    remo

--- Gradientes.py
+++
@@ -1,8 +0,0 @@
-def Menu_gradientes():
-    print("")
-    print("Programa en construccion xd")
-    print("Regresando al menu anterior")
-    print("")
-
-if __name__ == "__main__":
-    Menu_gradientes()
--- VAN.py
+++
@@ -1,8 +0,0 @@
-def Menu_VAN():
-    print("")
-    print("Programa en construccion xd")
-    print("Regresando al menu anterior")
-    print("")
-
-if __name__ == "__main__":
-    Menu_VAN()
